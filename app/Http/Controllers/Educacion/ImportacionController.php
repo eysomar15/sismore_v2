578d8702b5371e1c6bd99baeb51fa2c3ce5285ae
eysomar <eysomar_gambini@hotmail.com>
2022-07-12 11:48:23 -0500

    c

--- app/Http/Controllers/Educacion/ImportacionController.php
+++ app/Http/Controllers/Educacion/ImportacionController.php
@@ -34,7 +34,6 @@ class ImportacionController extends Controller
 
                 if ($data->estado == "APROBADO") {
                     switch ($data->codigo) {
-<<<<<<< HEAD
                         case('COD01'): $acciones = '<a href="PadronWeb/Aprobar/' . $data->id . '"   class="btn btn-info btn-sm"> <i class="fa fa-check"></i> </a>';break;
                         case('COD02'): $acciones = '<a href="CuadroAsigPersonal/Aprobar/' . $data->id . '"   class="btn btn-info btn-sm"> <i class="fa fa-check"></i> </a>';break;
                         case('COD03'): $acciones = '<a href="ECE/Importar/Aprobar/' . $data->id . '"   class="btn btn-info btn-sm"> <i class="fa fa-check"></i> </a>';break;                    
@@ -53,48 +52,6 @@ class ImportacionController extends Controller
                     }   
                 }          
                 
-=======
-                        default:
-                            $acciones = '&nbsp<button type="button" name="hy" class=" btn btn-secondary btn-sm"> <i class="fa fa-check"></i>  </button>';
-                            break;
-                    }
-                } else {
-                    switch ($data->codigo) {
-                        case ('COD01'):
-                            $acciones = '<a href="PadronWeb/Aprobar/' . $data->id . '"   class="btn btn-info btn-sm"> <i class="fa fa-check"></i> </a>';
-                            break;
-                        case ('COD02'):
-                            $acciones = '<a href="CuadroAsigPersonal/Aprobar/' . $data->id . '"   class="btn btn-info btn-sm"> <i class="fa fa-check"></i> </a>';
-                            break;
-                        case ('COD03'):
-                            $acciones = '<a href="ECE/Importar/Aprobar/' . $data->id . '"   class="btn btn-info btn-sm"> <i class="fa fa-check"></i> </a>';
-                            break;
-                        case ('COD06'):
-                            $acciones = '<a href="Censo/Aprobar/' . $data->id . '"   class="btn btn-info btn-sm"> <i class="fa fa-check"></i> </a>';
-                            break;
-                        case ('COD07'):
-                            $acciones = '<a href="Datass/Aprobar/' . $data->id . '"   class="btn btn-info btn-sm"> <i class="fa fa-check"></i> </a>';
-                            break;
-                        case ('COD08'):
-                            $acciones = '<a href="Matricula/Aprobar/' . $data->id . '"   class="btn btn-info btn-sm"> <i class="fa fa-check"></i> </a>';
-                            break;
-                        case ('COD09'):
-                            $acciones = '<a href="Tableta/Aprobar/' . $data->id . '"   class="btn btn-info btn-sm"> <i class="fa fa-check"></i> </a>';
-                            break;
-                        case ('COD10'):
-                            $acciones = '<a href="Matricula/AprobarConsolidadoAnual/' . $data->id . '"   class="btn btn-info btn-sm"> <i class="fa fa-check"></i> </a>';
-                            break;
-                        case ('COD11'):
-                            $acciones = '<a href="PEmapacopsa/Aprobar/' . $data->id . '"   class="btn btn-info btn-sm"> <i class="fa fa-check"></i> </a>';
-                            break;
-
-                        default:
-                            $acciones = '<a href="PadronWeb/AprobarNN/' . $data->id . '"   class="btn btn-info btn-sm"> <i class="fa fa-check"></i> </a>';
-                            break;
-                    }
-                }
-
->>>>>>> 9018859e4463b94b3503c13c5456445320eea05e
                 $acciones .= '&nbsp<button type="button" name="delete" id = "' . $data->id . '" class="delete btn btn-danger btn-sm">  <i class="fa fa-trash"></i> </button>';
                 return $acciones;
             })
