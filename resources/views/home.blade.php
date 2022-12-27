35fe3f126b6eccca61bdec622b2dce0465518f4e
eysomar <eysomar_gambini@hotmail.com>
2022-12-27 10:29:55 -0500

    uuuuuuu

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
-@extends('layouts.main',['titlePage'=>''])
-=======
 @extends('layouts.main', [
     'titlePage' => session('sistema_id') == 5 ? 'Principal' : 'Bienvenido al Sistema de Monitoreo Regional',
 ])
->>>>>>> d067d8f76abb7d088f6fc7e02c6760569d41b87d
 
 @section('content')
     @if (session('sistema_id') == 1)
